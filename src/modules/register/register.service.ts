ff2d62b0d1ffd63ac8972978b24a83b06db5e82e
gamage-hiru <susanthika551@gmail.com>
2023-03-01 18:58:44 +0530

    user register

--- src/modules/register/register.service.ts
+++ src/modules/register/register.service.ts
@@ -16,7 +16,29 @@ export class RegisterService {
   constructor(
     @InjectModel('register')
     private readonly registerModel: Model<RegisterDocument>,
-  ) {}
+  ) { }
+  // susanthika
+  async register(user: registerDto): Promise<register> {
 
- 
+    const checkMail = await this.registerModel.findOne({ email: user.email }).lean()
+    if (!isEmpty(checkMail)) {
+      throw new BadRequestException('Email already used');
+    }
+
+    const checkVehicleNum = await this.registerModel.findOne({ vehicleNumber: user.vehicleNumber }).lean()
+    if (!isEmpty(checkVehicleNum)) {
+      throw new BadRequestException('Vehicle Number already used');
+    }
+
+    const encryptedPassword = cryptr.encrypt(user.password);
+    // const decryptedString = cryptr.decrypt(encryptedString);
+    const newUser = new this.registerModel({
+      name: user.name,
+      email: user.email,
+      password: encryptedPassword,
+      vehicleNumber: user.vehicleNumber,
+      role: 'USER'
+    });
+    return newUser.save();
+  }
 }
